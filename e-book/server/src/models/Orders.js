f6c31dba11722c0da246e8b10b6935dda99abdfa
amod12 <yakhaamod6@gmail.com>
2023-07-06 12:25:25 +0545

    added order in server

---
+++ e-book/server/src/models/Orders.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const ordersSchema = new mongoose.Schema(
+  {
+    orders:[{
+      image:{type:String},
+      title: { type: String },
+      author: { type: String },
+      isbn: { type: String },
+      price: { type: Number },
+      description:{type: String},
+      catagory:{type: String},
+      status:{type: String},
+    },],  
+    orderStatus: { type: String, default:'Pending' },
+    name:{type: String},
+    userId:{type: String},
+    phone:{type: Number},
+    address:{type:String},
+    email: { type: String },
+    pickupDate:{type:String},
+    pickupTime:{type:String},
+    totalPrice: { type: Number },
+    quantity: { type: Number },
+
+
+  },
+  { collection: "orders" }
+);
+module.exports = mongoose.model("Orders", ordersSchema);
---
+++ e-book/server/src/routes/ordersRouter.js
@@ -0,0 +1,77 @@
+const express = require("express");
+const router = express.Router();
+const Orders = require("../models/Orders");
+const jwt = require('jsonwebtoken');
+
+router.post("/orders",  async (req, res) => {
+    try {
+        const dbResponse = await Orders.create(req.body)
+        if(dbResponse){
+            res.status(200).json({
+                msg: "orders dispatched successfully"
+            })
+        }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  router.get("/orders", async (req, res) => {
+    try {
+        const totalOrdersLength = await Orders.find()
+        if(req.query.userId){
+          const data = await Orders.find({"userId":req.query.userId})
+          res.status(200).json({
+            orders: data
+        })
+        }else{
+         const data = await Orders.find().limit(req.query.size).skip(req.query.size* req.query.page - req.query.size)
+          if(data){
+              res.status(200).json({
+                  orders:data,
+                  totalOrdersCount: totalOrdersLength.length
+              })
+          }
+        }
+       
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  router.delete("/orders", async (req, res) => {
+    try {
+      const totalOrdersLength = await Orders.find()
+      if(req.query.userId){
+        const data = await Orders.find({"userId":req.query.userId})
+        res.status(200).json({
+          orders: data
+      })
+      }else{
+            res.status(200).json({
+                orders:data,
+                totalOrdersCount: totalOrdersLength.length
+            })
+      }
+     
+  }  catch (err) {
+        console.log(err);
+    }
+    });
+
+    router.put("/orders", async (req, res) => {
+      try {
+        const data = await Orders.findByIdAndUpdate(req.body._id, req.body)
+        if(data){
+          res.status(200).json({msg: "updated successfully!"})
+        }
+        else{
+          res.status(500).json({msg:"something went wrong"})
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    });
+      
+
+module.exports = router;
